5762c65346e2251196e6b76389f9198666a846a1
shannonmcgurk <145011708+shannonmcgurk@users.noreply.github.com>
2024-03-28 13:30:06 +0000

    fixing minor image issues

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -119,5 +119,5 @@ def add_page(request, category_name_slug):
             else:
                 print(form.errors)
         
-        context_dict = {'form': form, 'category': category}
-        return render(request, 'rango/add_page.html', context=context_dict)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict)
