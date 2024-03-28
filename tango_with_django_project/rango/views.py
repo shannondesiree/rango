01f3afd063d9cd4246e4973400c97e2fe87b22a7
shannonmcgurk <145011708+shannonmcgurk@users.noreply.github.com>
2024-03-28 13:54:28 +0000

    completed chapter 8

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,21 +8,6 @@ from django.shortcuts import redirect
 from django.urls import reverse
 from rango.forms import PageForm
 
-# each view exists within the views.py file as a series of individual functions
-# each view takes in at least one argument - a Http Request object, which also lives in the django.http module
-# each view must return a http response object
-# for a user to see a view, you must map a URL to the view
-
-# view called index
-#def index(request):
-    # Construct a dictionary to pass to the template engine as its context.
-    # Note the key boldmessage matches to {{ boldmessage }} in the template!
- #   top_pages = Page.objects.order_by('-views')[:5]
-  #  category_list = Page.objects.order_by('-likes')[:5]
-   # context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
-    #context_dict['categories']=category_list
-    #context_dict['top_pages']=top_pages
-    #context_dict = {}
 def index(request):
     # Query the Page model for the top five most viewed pages
     top_pages = Page.objects.order_by('-views')[:5]
@@ -35,33 +20,20 @@ def index(request):
 
     return render(request, 'rango/index.html', context=context)
     
-    # Return a rendered response to send to the client.
-    # We make use of the shortcut function to make our lives easier.
-    # Note that the first parameter is the template we wish to use.
-    # return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Shannon'}
-    # return HttpResponse("Rango says here is the about page. <a href='/rango/'>Index</a>")
-    return render(request, 'rango/about.html', context=context_dict)
+    # prints out whether the method is a GET or a POST
+    print(request.method)
+    # prints out the user name, if no one is logged in it prints `AnonymousUser`
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
-    # Create a context dictionary which we can pass
-    # to the template rendering engine.
     context_dict = {}
     try:
-        # Can we find a category name slug with the given name?
-        # If we can't, the .get() method raises a DoesNotExist exception.
-        # The .get() method returns one model instance or raises an exception.
         category = Category.objects.get(slug=category_name_slug)
-        # Retrieve all of the associated pages.
-        # The filter() will return a list of page objects or an empty list.
         pages = Page.objects.filter(category=category)
-        # Adds our results list to the template context under name pages.
         context_dict['pages'] = pages
-        # We also add the category object from
-        # the database to the context dictionary.
-        # We'll use this in the template to verify that the category exists.
         context_dict['category'] = category
     except Category.DoesNotExist:
         # We get here if we didn't find the specified category.
