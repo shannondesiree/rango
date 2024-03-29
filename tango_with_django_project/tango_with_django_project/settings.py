ef510908827a239c641d283ba8f068b17206d3c1
shannonmcgurk <145011708+shannonmcgurk@users.noreply.github.com>
2024-03-29 10:39:10 +0000

    python anywhere deployment preparation

--- tango_with_django_project/tango_with_django_project/settings.py
+++ tango_with_django_project/tango_with_django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-f2)h%$8k$&iupa6spfpm_zfmybn)n)3e4k#!0b_9av+=qpr6)b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
